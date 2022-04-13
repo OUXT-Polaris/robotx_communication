34f9bb20b65db0ada889d99ed87748755b3a940e
AkubiCot <k.arai_payday@outlook.com>
2022-04-13 17:14:34 +0900

    :metal:add receive func(mock_server.c)

--- robotx_communication/src/mock_server.c
+++ robotx_communication/src/mock_server.c
@@ -1,17 +1,3 @@
-// Copyright (c) 2021 OUXT Polaris
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include <arpa/inet.h>  // for structs
 #include <stdio.h>
 #include <stdlib.h>      // for exit()
@@ -46,6 +32,20 @@ int bind_socket(int socket_1)
   return result;
 }
 
+// receive some messages
+void receive_messages(int socket_1)
+{
+  char buffer[64];
+
+  if(recv(socket_1, buffer, sizeof(buffer), 0) < 0){
+    printf("cannot get messages\n");
+  }
+  printf("%s\n", buffer);
+
+  close(socket_1);
+}
+
+
 int main()
 {
   int socket_1 = create_socket();
@@ -54,27 +54,28 @@ int main()
   int len;
 
   // bind socket
-  if (bind_socket(socket_1) < 0) {
+  if(bind_socket(socket_1) < 0){
     printf("cannot bind socket\n");
     exit(1);
   }
 
   // listen port
-  if (listen(socket_1, 1) < 0) {
+  if(listen(socket_1, 1) < 0){
     printf("cannot listen port\n");
     exit(1);
   }
 
   // connect w/ clients
   printf("connecting clients");
-  for (;;) {
+  for(;;){
     len = sizeof(client_addr);
     printf(".");
 
-    if (accept(socket_1, (struct sockaddr *)&client_addr, &len) == -1) {
+    if(accept(socket_1, (struct sockaddr *)&client_addr, &len) == -1){
       printf("cannot connect with clients\n");
     } else {
-      printf("connected with %s\n", inet_ntoa(client_addr.sin_addr));
+      printf("addr: %s\n", inet_ntoa(client_addr.sin_addr));
+      receive_messages(socket_1);
     }
   }
 
